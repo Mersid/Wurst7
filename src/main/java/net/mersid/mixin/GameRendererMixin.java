7d0267bcf16adb7e65b2b0cba307a1ce1f188a73
Mersid <mersid_email@yahoo.com>
2019-11-22 21:54:33 -0800

    Update GameRendererMixin.java

--- src/main/java/net/mersid/mixin/GameRendererMixin.java
+++ src/main/java/net/mersid/mixin/GameRendererMixin.java
@@ -1,5 +1,6 @@
 package net.mersid.mixin;
 
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,7 +28,7 @@ public class GameRendererMixin {
 	@Inject(at = @At("HEAD"),
 			method = "bobViewWhenHurt",
 			cancellable = true)
-	private void onBobViewWhenHurt(float partialTicks, CallbackInfo ci)
+	private void onBobViewWhenHurt(MatrixStack ms, float partialTicks, CallbackInfo ci)
 	{
 		CameraShakeEvent event = new CameraShakeEvent();
 		WurstClient.INSTANCE.getEventManager().fire(event);
