7ea169ce526a48a52ade6005e39aa654af5f1cf0
Mersid <mersid_email@yahoo.com>
2020-03-02 21:30:42 -0800

    Update AutoToolHack.java

--- src/main/java/net/mersid/hacks/AutoToolHack.java
+++ src/main/java/net/mersid/hacks/AutoToolHack.java
@@ -14,13 +14,12 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.util.math.BlockPos;
 import net.wurstclient.Category;
 import net.wurstclient.events.BlockBreakingProgressListener;
-import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.util.BlockUtils;
 import net.wurstclient.util.WItem;
 
-public class AutoToolHack extends Hack implements BlockBreakingProgressListener, LeftUpEventListener, UpdateListener {
+public class AutoToolHack extends Hack implements BlockBreakingProgressListener, LeftUpEventListener {
 	
 	private final CheckboxSetting useSwords = new CheckboxSetting("Use swords",
 			"Uses swords to break\n" + "leaves, cobwebs, etc.", false);
@@ -61,8 +60,6 @@ public class AutoToolHack extends Hack implements BlockBreakingProgressListener,
 	{
 		EVENTS.add(BlockBreakingProgressListener.class, this);
 		EVENTS.add(LeftUpEventListener.class, this);
-
-		EVENTS.add(UpdateListener.class, this);
 	}
 	
 	@Override
@@ -70,8 +67,6 @@ public class AutoToolHack extends Hack implements BlockBreakingProgressListener,
 	{
 		EVENTS.remove(BlockBreakingProgressListener.class, this);
 		EVENTS.remove(LeftUpEventListener.class, this);
-
-		EVENTS.remove(UpdateListener.class, this);
 	}
 	
 	/**
@@ -209,12 +204,5 @@ public class AutoToolHack extends Hack implements BlockBreakingProgressListener,
 		equipBestTool(pos, useSwords.isChecked(), useHands.isChecked(),
 				repairMode.isChecked());
 	}
-	long cwTimeMillis = 0;
-	@Override
-	public void onUpdate()
-	{
-		long nt = System.currentTimeMillis();
-		System.out.println(nt - cwTimeMillis);
-		cwTimeMillis = nt;
-	}
+
 }
